45590aaf2ffb561c32aab8249d362d6c863a0da3
Luiz Compan <Luizcompan2024@gmail.com>
2025-03-18 21:51:25 +0000

    alterações esteticas na exibição

--- Super-trunfo.C
+++ Super-trunfo.C
@@ -7,7 +7,7 @@ int main() {
     float area, pib;
     char cidade[50], estado[50], cod_card[4];
 
-    
+
     // Coletando dados do usuário
     printf("Crie uma carta\n\n");
 
@@ -31,13 +31,13 @@ int main() {
 
     // Exibindo os dados formatados
     printf("\n--- Dados da Carta 01 ---\n");
-    printf("Código da carta: %s\n", cod_card);
-    printf("Estado: %s\n", estado);
-    printf("Cidade: %s\n", cidade);
-    printf("Pontos Turísticos: %d\n", pts_Turisticos);
-    printf("População: %d\n", pop);
-    printf("Área: %.2f km²\n", area);
-    printf("PIB: %.2f\n", pib);
+    printf("|Código da carta: %s\n|", cod_card);
+    printf("|Estado: %s\n|", estado);
+    printf("|Cidade: %s\n|", cidade);
+    printf("|Pontos Turísticos: %d\n|", pts_Turisticos);
+    printf("|População: %d\n|", pop);
+    printf("|Área: %.2f km²\n|", area);
+    printf("|PIB: %.2f\n|", pib);
     
 
     return 0;
